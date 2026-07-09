2ec1fba131b4e0553f18bb486f7c3ac2e7dfdada
renhongtao2@gmail.com <renhongtao2@gmail.com>
2026-07-09 10:53:27 +0800

    add vercel.json rewrites to proxy API to backend

--- api/cards/metadata/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET(request) {
-  const url = new URL(request.url);
-  const backend = 'https://cardforge-backend-3.onrender.com';
-  const backendUrl = backend + url.pathname + url.search;
-  const resp = await fetch(backendUrl, { method: request.method, headers: request.headers, body: request.body });
-  const data = await resp.json();
-  return new Response(JSON.stringify(data), { status: 200, headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' } });
-}
